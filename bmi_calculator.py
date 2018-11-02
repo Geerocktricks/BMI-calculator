5858ca9e5a0e7023c69cdddf1d4429dfaf76bb1c
Geerocktricks <geerockface4@gmail.com>
2018-11-02 16:09:26 +0300

    Problem solved

---
+++ bmi_calculator.py
@@ -0,0 +1,7 @@
+print("Enter your weight in kgs")
+weight = float(input())
+print("Enter your height cm")
+height = float(input())
+def body_mass_index ():
+    print((weight/height/height)*10000)
+body_mass_index()
